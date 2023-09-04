3f77ce7eef2d111d223f9dbcea9ed86e125149fc
bagirov3min <bagirov3min@gmail.com>
2023-09-04 20:44:26 +0530

    Updated project structure. Fixed issues in tests 2-4

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -9,10 +9,10 @@ class TextPages(BasePage):
         expected_result = []
         result = []
 
-        self.element_is_visible(self.locators.BUTTON_FAVORITE).click()
+        self.element_is_clickable(self.locators.BUTTON_FAVORITE).click()
         expected_result.append(self.element_is_visible(self.locators.NAME_FROM_TITLE).text)
-        self.element_is_visible(self.locators.FAVORITES_MENU).click()
-        result.append(self.element_is_visible(self.locators.FAVORITE_NAME).text)
+        self.element_is_clickable(self.locators.FAVORITES_MENU).click()
+        result.append(self.element_is_visible(self.locators.FAVORITE_TITLE).text)
 
         return expected_result, result
 
@@ -23,7 +23,6 @@ class TextPages(BasePage):
         result = []
 
         for item in search:
-
             self.element_is_visible(self.locators.SEARCH_STRING).send_keys(item)
             self.element_is_clickable(self.locators.FIND_BUTTON).click()
             self.element_is_clickable(self.locators.FAVORITES_ADD).click()
@@ -35,6 +34,7 @@ class TextPages(BasePage):
 
         return expected_result, result
 
+    @property
     def add_from_title(self):
         search = ['самокаты', 'телефоны', 'одежда']
 
@@ -46,25 +46,26 @@ class TextPages(BasePage):
         for item in search:
 
             self.element_is_visible(self.locators.SEARCH_STRING).send_keys(item)
-            self.element_is_visible(self.locators.FIND_BUTTON).click()
-            self.element_is_visible(self.locators.TITLE).click()
+            self.element_is_clickable(self.locators.FIND_BUTTON).click()
+            self.element_is_clickable(self.locators.TITLE).click()
             all_windows = self.driver.window_handles
 
             for window in all_windows:
                 if window != main_window:
                     self.driver.switch_to.window(window)
-                    self.element_is_visible(self.locators.BUTTON_FAVORITE).click()
+                    self.element_is_clickable(self.locators.BUTTON_FAVORITE).click()
                     expected_result.append(self.element_is_visible(self.locators.NAME_FROM_TITLE).text)
                     self.driver.close()
                     self.driver.switch_to.window(main_window)
 
-            self.element_is_visible(self.locators.FAVORITES_MENU).click()
-            result.append(self.element_is_visible(self.locators.FAVORITE_NAME).text)
-            self.element_is_visible(self.locators.FAVORITE_DEL).click()
-            self.element_is_visible(self.locators.MAIN_PAGE).click()
+            self.element_is_clickable(self.locators.FAVORITES_MENU).click()
+            result.append(self.element_is_visible(self.locators.FAVORITE_TITLE).text)
+            self.element_is_clickable(self.locators.FAVORITE_DEL).click()
+            self.element_is_clickable(self.locators.MAIN_PAGE).click()
 
         return expected_result, result
 
+    @property
     def del_from_title(self):
         search = ['самокаты', 'телефоны', 'одежда']
 
@@ -75,23 +76,21 @@ class TextPages(BasePage):
 
         for item in search:
             self.element_is_visible(self.locators.SEARCH_STRING).send_keys(item)
-            self.element_is_visible(self.locators.FIND_BUTTON).click()
-            self.element_is_visible(self.locators.TITLE).click()
+            self.element_is_clickable(self.locators.FIND_BUTTON).click()
+            self.element_is_clickable(self.locators.TITLE).click()
             all_windows = self.driver.window_handles
 
             for window in all_windows:
                 if window != main_window:
                     self.driver.switch_to.window(window)
-                    self.element_is_visible(self.locators.BUTTON_FAVORITE).click()
+                    self.element_is_clickable(self.locators.BUTTON_FAVORITE).click()
                     self.driver.close()
                     self.driver.switch_to.window(main_window)
-
-            self.element_is_visible(self.locators.FAVORITES_MENU).click()
-            self.element_is_visible(self.locators.FAVORITE_TITLE).click()
-            self.element_is_visible(self.locators.BUTTON_FAVORITE).click()
-            self.element_is_visible(self.locators.MAIN_PAGE).click()
-
-        self.element_is_visible(self.locators.FAVORITES_MENU).click()
+            self.element_is_clickable(self.locators.FAVORITES_MENU).click()
+            self.element_is_clickable(self.locators.TITLE_FROM_FAVORITE_MENU).click()
+            self.element_is_clickable(self.locators.BUTTON_FAVORITE).click()
+            self.element_is_clickable(self.locators.MAIN_PAGE_FROM_FAVORITE).click()
+        self.element_is_clickable(self.locators.FAVORITES_MENU).click()
         result.append(self.element_is_visible(self.locators.FAVORITE_TEXT).text)
 
         return expected_result, result
